9e46858b931ce2ca1548c743664b2a0493a791d2
krilor <krilor@gmail.com>
2014-09-06 15:23:05 +0200

    Added email if solved.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kill a solitaire
+
+I wrote this script to try and solve a solitaire that never seemed to be solvable.
+It turned out to be almost unsolvable.
+
+> Not invluded in this repo is a file called **variables.py**.
+> It holds to variables, *email* and *password*.
--- solve.py
+++ solve.py
@@ -1,10 +1,39 @@
 import solitaire as sol
+import datetime
+import smtplib
+import variables as vars
 
-soli = sol.Solitaire()
-result = 0
+soli = sol.Solitaire() # Solitaire object
+result = 0 # For results
+
+sender = vars.email
+receivers = [vars.email]
+print "Run started at "  + datetime.datetime.now().time().isoformat()
 
 for i in xrange(1000000000):
   result = soli.solve()
   if result < 5:
-    print result
+    print "Run #" + repr(i) + ": " + repr(result) + " cards left."
+
+    if result = 0: # Solved
+
+      message = "From: Kristoffer <" + vars.email + """>
+To: Kristoffer Lorentsen <""" + vars.email + """>
+Subject: Solitaire solved!
+
+The Solitaire was solved on run #""" + repr(i) + " at " + datetime.datetime.now().time().isoformat()
+
+      try:
+        smtpObj = smtplib.SMTP('smtp.gmail.com',587)
+        smtpObj.ehlo()
+        smtpObj.starttls()
+        smtpObj.ehlo()
+        smtpObj.login(sender,vars.password)
+        smtpObj.sendmail(sender, receivers, message)
+        smtpObj.quit()
+        print "Successfully sent email"
+      except smtplib.SMTPException:
+        print "Error: unable to send email"
+
+
   soli.reset()
